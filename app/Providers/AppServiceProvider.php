72f1754a8e64db05dbdbfa4263dd12f71c910805
kahana128 <183961744+kahana128@users.noreply.github.com>
2025-03-14 20:02:53 +0900

    change https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if(\App::environment['production']) || (\App::environment['develop']){
+            \URL::forceScheme('https');
+        }
  
     }
 }
